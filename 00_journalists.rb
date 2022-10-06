616b7585a754d6349a3d95a6458afffd4f25100d
xavierpelle <xavierp34970@gmail.com>
2022-10-06 10:40:04 +0200

    a

---
+++ 00_journalists.rb
@@ -0,0 +1,113 @@
+ a = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+
+def number_account(a)
+    puts "Il y a : #{a.count} comptes twitter"
+end
+
+def shortest_account(a)
+    shortest = a.sort_by(&:length)
+    puts "Le nom de compte le plus court est #{shortest[0]}"
+end
+
+def number_5_caracter(a)
+    names = a.count {|x| x.length == 6}
+    puts "il y #{names} comptes qui ont 5 carrectères dans leurs pseudos"
+end
+
+def start_maj(a)
+    account_start_maj = a.count {|x| x[1].match?(/[A-Z]/)}
+    puts "Dans les comptes twitter il y a #{account_start_maj} compte qui commence avec une majuscule"
+
+end
+
+def list_a_to_z(a)
+    list = a.sort
+    puts list
+end
+
+def list_by_size(a)
+    list_size = a.sort_by(&:length)
+    puts list_size
+end
+
+def position_account(a)
+    position = a.index("@epenser")
+    puts "@epenser est a la #{position} places"
+end
+
+def handle_size(a)
+    trois = a.count {|a| a.length == 4}
+    quatre = a.count {|a| a.length == 5}
+    cinq= a.count {|a| a.length == 6}
+    six = a.count {|a| a.length == 7}
+    sept = a.count {|a| a.length == 8}
+    huit = a.count {|a| a.length == 9}
+    neuf = a.count {|a| a.length == 10}
+    dix = a.count {|a| a.length == 11}
+    onze = a.count {|a| a.length == 12}
+    douze = a.count {|a| a.length == 13}
+    treize = a.count {|a| a.length == 14}
+    quatorze= a.count {|a| a.length == 15}
+    quinze = a.count {|a| a.length == 16}
+    seize = a.count {|a| a.length > 17}
+
+
+    puts "il y a #{trois} nom de compte avec 3 carrectères"
+    puts "il y a #{quatre} noms de compte avec 4 carrectères"
+    puts "il y a #{cinq} noms de compte avec 5 carrectères"
+    puts "il y a #{six} noms de compte avec 6 carrectères"
+    puts "il y a #{sept} noms de compte avec 7 carrectères"
+    puts "il y a #{huit} noms de compte avec 8 carrectères"
+    puts "il y a #{neuf} noms de compte avec 9 carrectères"
+    puts "il y a #{dix} noms de compte avec 10 carrectères"
+    puts "il y a #{onze} noms de compte avec 11 carrectères"
+    puts "il y a #{douze} noms de compte avec 12 carrectères"
+    puts "il y a #{treize} noms de compte avec 13 carrectères"
+    puts "il y a #{quatorze} noms de compte avec 14 carrectères"
+    puts "il y a #{quinze} noms de compte avec 15 carrectères"
+    puts "il y a #{seize} nom de compte avec 32 carrectères"
+end
+
+def start
+    puts "Salut toi ! voici le SuperData3000 choisis ce que tu veux savoir !"
+    puts "1 - Combien il y a de compte twitter dans la SuperData3000"
+    puts "2 - Quelle est l'utlisateur qui a le nom le plus court !"
+    puts "3 - Combien de compte on leurs usermnames qui commence par une majuscule ?"
+    puts "4 - Trier les comptes par ordre alphabetique !"
+    puts "5 - Trier les comptes par ordre croissant !"
+    puts "6 - Quelle est la position de @Epenser dans la liste ? "
+    puts "7 - statistique mystère"
+    end 
+ 
+    start 
+
+    choix = gets.chomp.to_i
+
+    if  choix == 1 
+        number_account(a)
+
+    elsif choix == 2
+        shortest_account(a)
+
+    elsif choix == 3
+        number_5_caracter(a)
+
+    elsif choix == 4
+        start_maj(a)
+
+    elsif choix == 5
+        list_a_to_z(a)
+
+    elsif choix == 5
+        list_by_size(a)
+
+    elsif choix ==6
+        position_account(a)
+
+    elsif choix == 7
+        handle_size(a)
+            
+    end
+
+
